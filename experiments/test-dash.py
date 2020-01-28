601ca6d62dc64f07121e342a4341cd6b5217b5f7
Christophe Furmaniak <christophe.furmaniak@qima.com>
2020-01-28 11:05:09 +0100

    chore: update eclint config to deal with yaml in py files

--- experiments/test-dash.py
+++ experiments/test-dash.py
@@ -6,8 +6,6 @@ def strip_leading_double_space(stream):
     if stream.startswith("  "):
         stream = stream[2:]
     return stream.replace("\n  ", "\n")
-    # you could also do that on a line by line basis
-    # return "".join([s[2:] if s.startswith("  ") else s for s in stream.splitlines(True)])
 
 
 yaml = ruamel.yaml.YAML()
@@ -15,15 +13,15 @@ yaml = ruamel.yaml.YAML()
 # yaml.indent(mapping=2, sequence=2, offset=0)
 yaml.indent(mapping=2, sequence=4, offset=2)
 inp = """\
-  - op: replace
-    path: /x/y/z
-    value:
-      - name: dummy
-        property: fake
+- op: replace
+path: /x/y/z
+value:
+  - name: dummy
+    property: fake
 
-  - op: replace
-    path: /a/b/c
-    value: roger
+- op: replace
+  path: /a/b/c
+  value: roger
 """
 
 data = yaml.load(inp)
