7269599950555cfc5d44785cbabff09750b8e941
Christophe Furmaniak <christophe.furmaniak@qima.com>
2020-01-29 09:30:53 +0100

    fix: deal with dash inwards and lists

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 from setuptools import setup
 
-long_description = open('README.rst').read()
+long_description = open("README.rst").read()
+
+install_requires = ["ruamel.yaml>0.15"]
+if sys.version_info[:2] < (3, 4):
+    install_requires.append("pathlib")
 
 setup(
-    name='yamkix',
-    description='An opinionated yaml formatter based on ruamel.yaml',
+    name="yamkix",
+    description="An opinionated yaml formatter based on ruamel.yaml",
     long_description=long_description,
-    url='https://github.com/looztra/yamkix',
-    author='Christophe Furmaniak',
-    author_email='christophe.furmaniak@gmail.com',
-    version='0.5.0',
-    scripts=['yamkix'],
-    license='[Apache License 2.0](http: // www.apache.org / licenses /)',
-    install_requires=['ruamel.yaml>0.15'],
+    url="https://github.com/looztra/yamkix",
+    author="Christophe Furmaniak",
+    author_email="christophe.furmaniak@gmail.com",
+    version="0.5.0",
+    scripts=["yamkix"],
+    license="[Apache License 2.0](http: // www.apache.org / licenses /)",
+    install_requires=install_requires,
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
     ],
-    keywords='yaml format'
+    keywords="yaml format",
 )
