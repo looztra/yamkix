4983b6d98795d3acca624a8404f06687c0e40cc2
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2020-03-31 23:00:25 +0200

    feat: add the feature to print the installed version

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@
 
 from setuptools import setup
 
-long_description = open("README.rst").read()
+LONG_DESCRIPTION = open("README.rst").read()
 
-install_requires = ["ruamel.yaml>0.15"]
+INSTALL_REQUIRES = ["ruamel.yaml>0.16"]
 
 setup(
     name="yamkix",
     description="An opinionated yaml formatter based on ruamel.yaml",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     url="https://github.com/looztra/yamkix",
     author="Christophe Furmaniak",
     author_email="christophe.furmaniak@gmail.com",
     version="0.6.0",
     scripts=["yamkix"],
     license="[Apache License 2.0](http: // www.apache.org / licenses /)",
-    install_requires=install_requires,
+    install_requires=INSTALL_REQUIRES,
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
