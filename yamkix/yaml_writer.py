a3fd0797cc43b6b15e23e34431da606585fdb5fb
Christophe Furmaniak <christophe.furmaniak@qima.com>
2020-04-03 18:16:57 +0200

    fix(yaml_writer): set the line_width

--- yamkix/yaml_writer.py
+++ yamkix/yaml_writer.py
@@ -17,6 +17,7 @@ def get_opinionated_yaml_writer(yamkix_config: YamkixConfig,) -> YAML:
     yaml.explicit_end = yamkix_config.explicit_end
     yaml.default_flow_style = yamkix_config.default_flow_style
     yaml.preserve_quotes = yamkix_config.quotes_preserved
+    yaml.width = yamkix_config.line_width
     if yamkix_config.dash_inwards:
         yaml.indent(mapping=2, sequence=4, offset=2)
     return yaml
