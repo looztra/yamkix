ab7dae1cd1d3e9d88a3dca2ea99cb100af9f1214
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2021-12-20 18:00:04 +0100

    chore: make wait-for-py more explicit

--- wait-for-pypi.sh
+++ wait-for-pypi.sh
@@ -5,9 +5,10 @@ while true; do
   date
   pip install "yamkix==${YAMKIX_VERSION}" || true
   if hash yamkix; then
+    echo "Found expected version, let's go on"
     break
   else
-    echo "Did not find the expected version [${YAMKIX_VERSION}], sleeping"
+    echo "Did not find the expected version [${YAMKIX_VERSION}], sleeping 15s"
     sleep 15s
   fi
 done
