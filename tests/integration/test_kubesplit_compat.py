c709696c4792833f896a85246930ce07385ae9a2
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2025-08-13 16:14:24 +0200

    tests(integration): provide first kubesplit compat tests

--- tests/integration/test_kubesplit_compat.py
+++ tests/integration/test_kubesplit_compat.py
@@ -1,4 +1,4 @@
-"""Provide tests that verify we can install the package."""
+"""Provide integration tests related to kubesplit compatibility."""
 
 import subprocess
 from pathlib import Path
@@ -7,30 +7,31 @@ import pytest
 
 
 @pytest.mark.integration
-class TestPackageInstallation:
-    """Provide integration tests for package installation."""
+class TestKubesplitCompatibility:
+    """Provide integration tests for kubesplit compatibility."""
 
-    @pytest.mark.parametrize(
-        "resolution",
-        [
-            pytest.param("highest", id="highest"),
-            pytest.param("lowest", id="lowest"),
-            pytest.param("lowest-direct", id="lowest-direct"),
-        ],
-    )
-    def test_yamkix_can_be_installed_from_dist(  # noqa: PLR0913
+    def test_kubesplit_version(
         self,
         wheel_file: Path,
         path_to_uv: str,
         uv_tool_dir: Path,
         install_env: dict[str, str],
-        resolution: str,
         project_path: Path,
     ) -> None:
-        """Test that yamkix can be installed from the distribution."""
-        tool_name = "yamkix"
+        """Test we can install kubesplit and get its version."""
+        tool_name = "kubesplit"
         result_tool_install = subprocess.run(  # noqa: S603
-            [path_to_uv, "tool", "install", "--no-config", "--resolution", resolution, str(wheel_file)],
+            [
+                path_to_uv,
+                "tool",
+                "install",
+                "--no-config",
+                "--find-links",
+                wheel_file.parent,
+                "--prerelease",
+                "allow",
+                "kubesplit",
+            ],
             check=False,
             cwd=project_path,
             capture_output=True,
@@ -43,13 +44,12 @@ class TestPackageInstallation:
         )
         assert (uv_tool_dir / tool_name).exists(), f"UV {tool_name} bin directory should exist"
         result_tool_run = subprocess.run(  # noqa: S603
-            [str(uv_tool_dir / tool_name / "bin" / tool_name), "--help"],
+            [str(uv_tool_dir / tool_name / "bin" / tool_name), "--version"],
             check=False,
             capture_output=True,
             text=True,
         )
         assert result_tool_run.returncode == 0, f"Installed {tool_name} executable is not functional"
-        # Let's uninstall the package after testing because we want to be able to install it again during the session
         result_tool_uninstall = subprocess.run(  # noqa: S603
             [path_to_uv, "tool", "uninstall", "--no-config", tool_name],
             check=False,
