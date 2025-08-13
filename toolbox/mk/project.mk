7847a451f7404ad4879ac3db298726de9b2d0b7b
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2025-08-13 11:52:41 +0200

    ci(integration): move integration tests in a dedicated job

--- toolbox/mk/project.mk
+++ toolbox/mk/project.mk
@@ -2,3 +2,13 @@
 test-python-versions: ## ▶ Run tests for all supported python versions
 	@echo "Running tests for all supported python versions"
 	@uv run tox run
+
+.PHONY: build-docs
+build-docs: ## ▶ Build the documentation
+	@echo "Building the documentation"
+	@uv run mkdocs build
+
+.PHONY: serve-docs
+serve-docs: ## ▶ Serve the documentation
+	@echo "Serving the documentation"
+	@uv run mkdocs serve
