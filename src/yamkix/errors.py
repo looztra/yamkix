ae1b857f7c99a680818163c8efd4f17a2266bdfa
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2025-08-15 21:07:56 +0200

    feat: raise an error if something goes wrong during round trip

--- src/yamkix/errors.py
+++ src/yamkix/errors.py
@@ -7,3 +7,11 @@ class InvalidTypValueError(ValueError):
     def __init__(self, typ: str) -> None:
         """Create a new instance of InvalidTypValueError."""
         super().__init__(f"'{typ}' is not a valid value for option --typ. Allowed values are 'safe' and 'rt'")
+
+
+class InvalidYamlContentError(ValueError):
+    """Exception raised for invalid YAML content."""
+
+    def __init__(self) -> None:
+        """Initialize InvalidYamlContentError."""
+        super().__init__("Invalid YAML content")
